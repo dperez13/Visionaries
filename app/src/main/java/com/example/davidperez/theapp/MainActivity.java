1b6066ea8426d75e7ad6660b4c469accb1ade2fa
dperez13 <davidpe131@gmail.com>
2015-02-17 00:13:56 -0500

    Working on transition between screens.

--- app/src/main/java/com/example/davidperez/theapp/MainActivity.java
+++ app/src/main/java/com/example/davidperez/theapp/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,8 +14,32 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        //Here we have some code to switch activities on the click of a button. We may have to refactor later to a better design(i.e. call a function that has a single listener that waits for many signals
+        Button newRoute = (Button) findViewById(R.id.newRouteButton);
+        newRoute.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), new_route.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+        Button pref = (Button) findViewById(R.id.prefButton);
+        pref.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), preferences.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+        Button saved = (Button) findViewById(R.id.savedRoutes);
+        saved.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), saved_routes.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
